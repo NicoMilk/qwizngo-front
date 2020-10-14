ce0b8abae8390fdc60af1f37612cb9a431f2e270
fatymata <fatymata.gassama@epitech.eu>
2020-10-14 16:59:24 +0200

    add prod mode to server.js

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+} else {
+  app.use(serveStatic(path.join(__dirname, 'dist')))
+}
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 var port = process.env.PORT || 5000
 
 app.listen(port)
