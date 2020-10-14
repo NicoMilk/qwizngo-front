1b57e715a9ed03bc82cc1622687b9c4f76f9d723
Francois Annabi <francois.annabi@epitech.eu>
2020-10-14 16:10:49 +0200

    register toast

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,7 +6,9 @@
           <h4 class>Enregistrer un compte</h4>
           <div class="py-4">
             <div class="form-group row">
-              <label for="name" class="col-md-4 col-form-label text-md-right">Nom</label>
+              <label for="name" class="col-md-4 col-form-label text-md-right"
+                >Nom</label
+              >
               <div class="col-md-6">
                 <input
                   v-model="form.name"
@@ -21,7 +23,9 @@
               </div>
             </div>
             <div class="form-group row">
-              <label for="email" class="col-md-4 col-form-label text-md-right">Adresse E-mail</label>
+              <label for="email" class="col-md-4 col-form-label text-md-right"
+                >Adresse E-mail</label
+              >
               <div class="col-md-6">
                 <input
                   v-model="form.email"
@@ -35,7 +39,9 @@
               </div>
             </div>
             <div class="form-group row">
-              <label for="pass1" class="col-md-4 col-form-label text-md-right">Mot de passe</label>
+              <label for="pass1" class="col-md-4 col-form-label text-md-right"
+                >Mot de passe</label
+              >
               <div class="col-md-6">
                 <input
                   v-model="form.password"
@@ -48,10 +54,9 @@
               </div>
             </div>
             <div class="form-group row">
-              <label
-                for="pass2"
-                class="col-md-4 col-form-label text-md-right"
-              >Confirmer le mot de passe</label>
+              <label for="pass2" class="col-md-4 col-form-label text-md-right"
+                >Confirmer le mot de passe</label
+              >
               <div class="col-md-6">
                 <input
                   v-model="form.password2"
@@ -67,19 +72,17 @@
               type="submit"
               @click.prevent="register"
               class="btn btn-primary my-4"
-            >Créer un compte</button>
+            >
+              Créer un compte
+            </button>
           </div>
-          <div
-            v-if="errors ==1"
-            class="alert alert-danger mt-2"
-            role="alert"
-          >Tous les champs doivent être remplis</div>
+          <div v-if="errors == 1" class="alert alert-danger mt-2" role="alert">
+            Tous les champs doivent être remplis
+          </div>
         </div>
-        <div
-          v-if="errors ==2"
-          class="alert alert-danger mt-2"
-          role="alert"
-        >Cet email existe déjà, veuillez en entrer un nouveau</div>
+        <div v-if="errors == 2" class="alert alert-danger mt-2" role="alert">
+          Cet email existe déjà, veuillez en entrer un nouveau
+        </div>
       </div>
     </div>
   </div>
@@ -96,10 +99,18 @@ export default {
       name: "",
       email: "",
       password: "",
-      password2: ""
-    }
+      password2: "",
+    },
   }),
   methods: {
+    toast(title, message, faulty = false) {
+      this.$root.$bvToast.toast(message, {
+        title: title,
+        toaster: "b-toaster-top-center",
+        variant: faulty ? "danger" : "success",
+        appendToast: true,
+      });
+    },
     register() {
       if (
         this.form.name &&
@@ -108,23 +119,21 @@ export default {
         this.form.password2
       ) {
         if (this.form.password == this.form.password2) {
-           //const bcrypt = require("bcrypt"); //
-           //const hashedPwd = bcrypt.hash(password, 10); //
-           //this.form.password = hashedPwd; //
+          //const bcrypt = require("bcrypt"); //
+          //const hashedPwd = bcrypt.hash(password, 10); //
+          //this.form.password = hashedPwd; //
           User.register(this.form)
             .then(() => {
               this.$router.push({ name: "Login" });
             })
-            .catch(error => {
-              this.errors = 2;
-
-              this.error_message = error.response;
+            .catch((error) => {
+              this.toast("Erreur!", error.message, true);
             });
         }
       } else {
         this.errors = 1;
       }
-    }
-  }
+    },
+  },
 };
 </script>
