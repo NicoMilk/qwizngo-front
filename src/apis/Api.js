94246dc50af9ec0170d64bab69da7048da0cd05c
fatymata <fatymata.gassama@epitech.eu>
2020-10-14 13:03:59 +0200

    change base url name

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let baseApi = axios.create({
   //baseURL: "https://qwiz-go-api.herokuapp.com",
-  baseURL: process.env.BASE_URL
+  baseURL: process.env.MYBASE_URL
 });
 
 let Api = function () {
