1e21f614cbf06eb5f867ee437fbea6e45dc652ae
fatymata <fatymata.gassama@epitech.eu>
2020-10-14 17:46:42 +0200

    adding app.get dist

--- server.js
+++ server.js
@@ -7,5 +7,9 @@ var app = express()
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000
 
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, '/dist', 'index.html'));
+});
+
 app.listen(port)
 console.log('server started on port ' + port)
